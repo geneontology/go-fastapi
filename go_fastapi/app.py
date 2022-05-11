bd563025a64184626802e8e4fc8bd59aed4fd523
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-05-11 14:09:23 -0700

    add parallel splash page

--- go_fastapi/app.py
+++ go_fastapi/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 import logging
 from .routers import slimmer
@@ -7,10 +8,7 @@ log = logging.getLogger(__name__)
 app = FastAPI()
 app.include_router(slimmer.router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
     uvicorn.run('app:app', host="0.0.0.0", port=80)
