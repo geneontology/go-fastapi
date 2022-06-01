4c53b0a6d2a839fd1bdfbc995e32c7b8e00d9765
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-06-01 14:19:02 -0700

    add back in doc

--- app/routers/slimmer.py
+++ app/routers/slimmer.py
@@ -23,10 +23,11 @@ class RelationshipType(str, Enum):
 
 @router.get("/api/bioentityset/slimmer/function", tags=["bioentityset/slimmer"])
 async def slimmer_function(slim: List[str] = Query(..., help="Map objects up (slim) to a higher level category. "
-                                                              "Value can be ontology class ID",
-                                                    description="example: GO:0005575"),
+                                                             "Value can be ontology class ID",
+                                                   description="example: GO:0005575"),
                            subject: List[str] = Query(..., description="example: ZFIN:ZDB-GENE-980526-388"),
-                           relationship_type: RelationshipType = Query(default=RelationshipType.acts_upstream_of_or_within),
+                           relationship_type: RelationshipType = Query(
+                               default=RelationshipType.acts_upstream_of_or_within),
                            exclude_automatic_assertions: bool = False,
                            rows: int = 100, start: int = 1):
     """
