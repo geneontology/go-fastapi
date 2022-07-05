6b0adbf9daad128ee4159909293c3a6e4c3f1f31
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-07-05 12:43:15 -0700

    fixing tests

--- tests/test_endpoints_are_returning.py
+++ tests/test_endpoints_are_returning.py
@@ -7,7 +7,7 @@ test_client = TestClient(app)
 
 
 gene_ids = ["ZFIN:ZDB-GENE-980526-388", "ZFIN:ZDB-GENE-990415-8"]
-go_ids = ["GO:0008150", "GO:0008149"]
+go_ids = ["GO:0008150"]
 subsets = ["goslim_agr"]
 shared_ancestors = [("GO:0006259", "GO:0046483")]
 
