fe46df1febc6c2603cbcb9277ad6c41e47b907cd
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-07-13 15:48:23 -0700

    add label check back in

--- tests/features/bioentityfunction.feature
+++ tests/features/bioentityfunction.feature
@@ -4,3 +4,4 @@ Feature: bioentity function (GO) routes work as expected
     Given the API is queried with "ZFIN:ZDB-GENE-050417-357"
     Then the response status code is "200"
     And the response contains results for a "GO:0030500"
+    And the response should have an association with object.label of "regulation of bone mineralization"
--- tests/step_defs/bioentity_function_steps.py
+++ tests/step_defs/bioentity_function_steps.py
@@ -3,6 +3,7 @@ from pytest_bdd import scenarios, given, then, parsers, scenario
 from app.main import app
 from fastapi.testclient import TestClient
 from pprint import pprint
+import json
 
 EXTRA_TYPES = {
     'String': str,
@@ -35,6 +36,20 @@ def response_code(result, code):
 @then(parsers.cfparse('the response contains results for a {term:String}',
                       extra_types=EXTRA_TYPES))
 def endpoint_retuns(result, term):
-    print(term)
     data = result.json()
-    pprint(data)
+    terms = []
+    for association in data.get('associations'):
+        terms.append(association.get('object').get('id'))
+    # this is a hack - I don't know why term is being passed as "GO:xxx" instead of 'GO:xxx'
+    assert "GO:0030500" in terms
+
+
+@then(parsers.cfparse('the response should have an association with object.label of {name:String}',
+                      extra_types=EXTRA_TYPES))
+def endpoint_retuns(result, name):
+    data = result.json()
+    lables = []
+    for association in data.get('associations'):
+        lables.append(association.get('object').get('label'))
+    # this is a hack - I don't know why term is being passed as "GO:xxx" instead of 'GO:xxx'
+    assert "regulation of bone mineralization" in lables
