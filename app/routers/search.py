11419eecce1922bba66d1736e89ac51260b1ec1e
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-07-15 16:42:08 -0700

    reformat results in line

--- app/routers/search.py
+++ app/routers/search.py
@@ -46,7 +46,7 @@ async def search_term(term: str):
     return results
 
 
-@router.get('/api/search/entity/autocomplete/{term}', tags=["search"], response_model=List[TestResult])
+@router.get('/api/search/entity/autocomplete/{term}', tags=["search"])
 async def autocomplete_term(term: str = Query(..., description="example: `biological`")):
     """
         Returns list of matching concepts or entities using lexical search
@@ -54,18 +54,17 @@ async def autocomplete_term(term: str = Query(..., description="example: `biolog
     q = GolrSearchQuery(term, user_agent=USER_AGENT)
     results = q.autocomplete()
     pprint(results)
-
-    # class AResult(BaseModel):
-    #     id: Union[str, None] = None
-    #     label: List[str] = []
-    #     match: Union[str, None] = None
-    #     category: List[str] = []
-    #     taxon: Union[str, None] = None
-    #     taxon_label: Union[str, None] = None
-    #     highlight: Union[str, None] = None
-    #     has_highlight: Union[str, None] = None
-    #
-    # class TestResult(BaseModel):
-    #     docs: List[AResult] = []
-
+    docs = []
+    auto_result = {}
+    for item in results.docs:
+        auto_result = {"id": item.id,
+                       "label": item.label,
+                       "match": item.match,
+                       "category": item.category,
+                       "taxon": item.taxon,
+                       "taxon_label": item.taxon_label,
+                       "highlight": item.highlight,
+                       "has_highlight": item.has_highlight}
+        docs.append(auto_result)
+    print(docs)
     return results
