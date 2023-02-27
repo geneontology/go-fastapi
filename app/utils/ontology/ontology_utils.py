d94777afb5e9a5ba290de392f16e69e5a01becd5
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-02-27 13:13:15 -0800

    convert hegroup sparql endpoint to go using oaklib

--- app/utils/ontology/ontology_utils.py
+++ app/utils/ontology/ontology_utils.py
@@ -214,7 +214,6 @@ def create_go_summary_sparql(goid):
     goid = correct_goid(goid)
     return (
         """
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX definition: <http://purl.obolibrary.org/obo/IAO_0000115>
     PREFIX obo: <http://www.geneontology.org/formats/oboInOwl#>
 
