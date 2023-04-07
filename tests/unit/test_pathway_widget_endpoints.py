f2d8ab46c3853445c0dc403446ecc04ebb79418c
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-04-07 16:02:35 -0700

    add more test data

--- tests/unit/test_pathway_widget_endpoints.py
+++ tests/unit/test_pathway_widget_endpoints.py
@@ -7,11 +7,8 @@ from app.main import app
 
 test_client = TestClient(app)
 
-gene_ids = ["ZFIN:ZDB-GENE-980526-388", "ZFIN:ZDB-GENE-990415-8", "MGI:3588192"]
-go_ids = ["GO:0008150"]
-subsets = ["goslim_agr"]
-shared_ancestors = [("GO:0006259", "GO:0046483")]
-uris = ["http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FGO_0008150"]
+gene_ids = ["ZFIN:ZDB-GENE-980526-388", "WB:WBGene00002147",
+            "ZFIN:ZDB-GENE-990415-8", "MGI:3588192", "FB:FBgn0003731"]
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +16,7 @@ logger = logging.getLogger(__name__)
 def test_get_gocams_by_geneproduct_id():
     id = urllib.parse.quote("MGI:3588192")
     response = test_client.get(f"/api/gp/{id}/models")
-    assert len(response.json()) == 2
+    assert len(response.json()) > 0
     assert response.status_code == 200
 
 
