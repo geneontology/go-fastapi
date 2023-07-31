26f5a3d97fda8b544724a1c68044d8cdd687ae95
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-07-31 14:33:36 -0700

    more linting

--- tests/integration/step_defs/slimmer_steps.py
+++ tests/integration/step_defs/slimmer_steps.py
@@ -10,12 +10,12 @@ EXTRA_TYPES = {
 
 @scenario("../features/slimmer.feature", "slimmer routes work as expected")
 def test_slim():
-    # boilerplate
+    """
+    Scenario: slimmer routes work as expected
+    """
     pass
 
-
 # Given Steps
-
 
 @given(
     parsers.cfparse(
@@ -25,6 +25,17 @@ def test_slim():
     target_fixture="result",
 )
 def api_result(bioentity_id, slimterms):
+    """
+    Given the endpoint is queried with gene {bioentity_id} and slim {slimterms}.
+
+    :param bioentity_id: The bioentity ID to be queried.
+    :type bioentity_id: str
+    :param slimterms: Comma-separated slim terms to be used for querying.
+    :type slimterms: str
+
+    :return: The response obtained after querying the API endpoint.
+    :rtype: TestResponse
+    """
     test_client = TestClient(app)
     slimterms = slimterms.replace('"', "")
     slimterms_list = slimterms.split(",")
@@ -35,9 +46,16 @@ def api_result(bioentity_id, slimterms):
 
 # Then Steps
 
-
 @then(parsers.parse('the response status code is "{code:d}"'))
 def response_code(result, code):
+    """
+    Then the response status code is {code}.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param code: The expected status code.
+    :type code: int
+    """
     assert result.status_code == code
 
 
@@ -48,6 +66,14 @@ def response_code(result, code):
     )
 )
 def subject_label(result, bioentity_label):
+    """
+    Then the response should have an association with subject.label of {bioentity_label}.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param bioentity_label: The expected bioentity label.
+    :type bioentity_label: str
+    """
     data = result.json()
     found_it = False
     bioentity_label = bioentity_label.replace('"', "")
@@ -66,6 +92,14 @@ def subject_label(result, bioentity_label):
     )
 )
 def subject_id(result, bioentity_id):
+    """
+    Then the response contains an association with subject.id of {bioentity_id}.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param bioentity_id: The expected bioentity ID.
+    :type bioentity_id: str
+    """
     data = result.json()
     found_it = False
     bioentity_id = bioentity_id.replace('"', "")
@@ -78,6 +112,14 @@ def subject_id(result, bioentity_id):
 
 @then(parsers.cfparse("the response should have {term:String} in the slim", extra_types=EXTRA_TYPES))
 def term_in_slim(result, term):
+    """
+    Then the response should have {term} in the slim.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param term: The expected slim term.
+    :type term: str
+    """
     data = result.json()
     found_it = False
     term = term.replace('"', "").strip()
