1796beada600c78bc3c0f8789b9c435ce03b119b
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-07-31 13:58:18 -0700

    refactor to use unittest instead of assert directly

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,14 +1,12 @@
 import logging
-
+import unittest
 from curies import Converter
 from fastapi.testclient import TestClient
 from prefixmaps import load_context
-
 from app.main import app
 from app.utils.prefixes.prefix_utils import remap_prefixes
 
 test_client = TestClient(app)
-
 logger = logging.getLogger(__name__)
 
 gene_ids = ["ZFIN:ZDB-GENE-980526-388", "ZFIN:ZDB-GENE-990415-8"]
@@ -17,11 +15,23 @@ subsets = ["goslim_agr"]
 shared_ancestors = [("GO:0006259", "GO:0046483")]
 
 
-def test_prefix_utils():
-    context = load_context("go")
-    extended_prefix_map = context.as_extended_prefix_map()
-    converter = Converter.from_extended_prefix_map(extended_prefix_map)
-    cmaps = converter.prefix_map
-    # hacky solution to: https://github.com/geneontology/go-site/issues/2000
-    cmaps = remap_prefixes(cmaps)
-    assert cmaps["MGI"] == "http://identifiers.org/mgi/MGI:"
+class TestPrefixUtils(unittest.TestCase):
+
+    def test_prefix_utils(self):
+        """
+        Test the prefix utilities.
+
+        :return: None
+        """
+        context = load_context("go")
+        extended_prefix_map = context.as_extended_prefix_map()
+        converter = Converter.from_extended_prefix_map(extended_prefix_map)
+        cmaps = converter.prefix_map
+        # hacky solution to: https://github.com/geneontology/go-site/issues/2000
+        cmaps = remap_prefixes(cmaps)
+        self.assertEqual(cmaps["MGI"], "http://identifiers.org/mgi/MGI:")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
