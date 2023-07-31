5aae2438f1862e254b53c9fb4f097444a2bad687
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-07-31 14:35:30 -0700

    more linting

--- tests/integration/step_defs/bioentity_function_steps.py
+++ tests/integration/step_defs/bioentity_function_steps.py
@@ -1,50 +1,54 @@
 from pprint import pprint
-
 from fastapi.testclient import TestClient
 from pytest_bdd import given, parsers, scenario, then
-
 from app.main import app
 
 EXTRA_TYPES = {
     "String": str,
 }
 
-
 @scenario("../features/bioentityfunction.feature", "test function endpoint")
 def test_zfin():
+    """
+    Scenario: test function endpoint
+    """
     # boilerplate
     pass
 
-
 @scenario(
     "../features/bioentityfunction.feature",
-    "User fetches all GO functional assignments for a human gene " "using an HGNC id",
+    "User fetches all GO functional assignments for a human gene using an HGNC id",
 )
 def test_hgnc():
+    """
+    Scenario: User fetches all GO functional assignments for a human gene using an HGNC id
+    """
     # boilerplate
     pass
 
-
 @scenario(
     "../features/bioentityfunction.feature",
-    "User fetches all GO functional assignments for a " "human gene using a NCBI ID",
+    "User fetches all GO functional assignments for a human gene using a NCBI ID",
 )
 def test_ncbi():
+    """
+    Scenario: User fetches all GO functional assignments for a human gene using a NCBI ID
+    """
     # boilerplate
     pass
-
 
 @scenario(
     "../features/bioentityfunction.feature",
-    "User fetches GO functional assignments and " "wishes to filter negated results",
+    "User fetches GO functional assignments and wishes to filter negated results",
 )
 def test_negated():
+    """
+    Scenario: User fetches GO functional assignments and wishes to filter negated results
+    """
     # boilerplate
     pass
 
-
 # Given Steps
-
 
 @given(
     parsers.cfparse(
@@ -54,18 +58,33 @@ def test_negated():
     target_fixture="result",
 )
 def api_result(bioentity_id):
+    """
+    Given the "{endpoint}" is queried with "{bioentity_id}".
+
+    :param endpoint: The API endpoint to be queried.
+    :type endpoint: str
+    :param bioentity_id: The bioentity ID to be used in the query.
+    :type bioentity_id: str
+    :return: The response obtained after querying the API endpoint.
+    :rtype: TestResponse
+    """
     test_client = TestClient(app)
     response = test_client.get(f"/api/bioentity/gene/{bioentity_id}/function")
     return response
 
-
 # Then Steps
-
 
 @then(parsers.parse('the response status code is "{code:d}"'))
 def response_code(result, code):
-    assert result.status_code == code
+    """
+    Then the response status code is {code}.
 
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param code: The expected status code.
+    :type code: int
+    """
+    assert result.status_code == code
 
 @then(
     parsers.cfparse(
@@ -74,6 +93,14 @@ def response_code(result, code):
     )
 )
 def endpoint_returns(result, term):
+    """
+    Then the response contains an association with object.id of {term}.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param term: The expected term value to be present in the response.
+    :type term: str
+    """
     data = result.json()
     found_it = False
     term = term.replace('"', "")
@@ -83,7 +110,6 @@ def endpoint_returns(result, term):
             found_it = True
     assert found_it
 
-
 @then(
     parsers.cfparse(
         "the response should have an association with object.label of {name:String}",
@@ -91,6 +117,14 @@ def endpoint_returns(result, term):
     )
 )
 def endpoint_returns(result, name):
+    """
+    Then the response should have an association with object.label of {name}.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param name: The expected name value to be present in the response.
+    :type name: str
+    """
     data = result.json()
     found_it = False
     for association in data.get("associations"):
@@ -99,7 +133,6 @@ def endpoint_returns(result, name):
             found_it = True
     assert found_it
 
-
 @then(
     parsers.cfparse(
         "the response should have an association with qualifiers of {qualifier:String}",
@@ -107,6 +140,14 @@ def endpoint_returns(result, name):
     )
 )
 def endpoint_retuns(result, qualifier):
+    """
+    Then the response should have an association with qualifiers of {qualifier}.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param qualifier: The expected qualifier value to be present in the response.
+    :type qualifier: str
+    """
     data = result.json()
     found_it = False
     for association in data.get("associations"):
@@ -117,7 +158,6 @@ def endpoint_retuns(result, qualifier):
                     found_it = True
     assert found_it
 
-
 @then(
     parsers.cfparse(
         "the response should have an association with associations.negated is {qualifier:String}",
@@ -125,6 +165,14 @@ def endpoint_retuns(result, qualifier):
     )
 )
 def endpoint_retuns(result, qualifier):
+    """
+    Then the response should have an association with associations.negated is {qualifier}.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param qualifier: The expected qualifier value to be present in the response.
+    :type qualifier: str
+    """
     data = result.json()
     found_it = False
     for association in data.get("associations"):
--- tests/integration/step_defs/prefixes_steps.py
+++ tests/integration/step_defs/prefixes_steps.py
@@ -1,45 +1,44 @@
 from pprint import pprint
-
 from fastapi.testclient import TestClient
 from pytest_bdd import given, parsers, scenario, then
-
 from app.main import app
 
 EXTRA_TYPES = {
     "String": str,
 }
 
-
 @scenario("../features/prefixes.feature", "Client code requires list of all prefixes in use")
 def test_identifiers():
+    """
+    Scenario: Client code requires list of all prefixes in use
+    """
     # boilerplate
     pass
-
-
-#
-# @scenario('../features/prefixes.feature', 'Contract a GO URI to a GO OBO-style ID')
-# def test_go_obo_style_uri():
-#     # boilerplate
-#     pass
-#
-#
-
 
 @scenario("../features/prefixes.feature", "Expand a GO ID to a URI")
 def test_exand_uri():
+    """
+    Scenario: Expand a GO ID to a URI
+    """
     # boilerplate
     pass
-
 
 @given(
     parsers.cfparse("the {endpoint:String} is queried", extra_types=EXTRA_TYPES),
     target_fixture="result",
 )
 def api_result(endpoint):
+    """
+    Given the {endpoint} is queried.
+
+    :param endpoint: The API endpoint to be queried.
+    :type endpoint: str
+    :return: The response obtained after querying the API endpoint.
+    :rtype: TestResponse
+    """
     test_client = TestClient(app)
     response = test_client.get("/api/identifier/prefixes")
     return response
-
 
 @given(
     parsers.cfparse(
@@ -49,6 +48,16 @@ def api_result(endpoint):
     target_fixture="result",
 )
 def api_result(endpoint, thing):
+    """
+    Given the "{endpoint}" endpoint is queried with "{thing}".
+
+    :param endpoint: The API endpoint to be queried.
+    :type endpoint: str
+    :param thing: The thing to be used in the query.
+    :type thing: str
+    :return: The response obtained after querying the API endpoint.
+    :rtype: TestResponse
+    """
     test_client = TestClient(app)
     print("")
     print(endpoint + thing)
@@ -56,16 +65,29 @@ def api_result(endpoint, thing):
     pprint(response.json())
     return response
 
-
 # Then Steps
-
 
 @then(parsers.parse('the response status code is "{code:d}"'))
 def response_code(result, code):
-    assert result.status_code == code
+    """
+    Then the response status code is {code}.
 
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param code: The expected status code.
+    :type code: int
+    """
+    assert result.status_code == code
 
 @then(parsers.cfparse('the content should contain "{content:String}"', extra_types=EXTRA_TYPES))
 def contains(result, content):
+    """
+    Then the content should contain {content}.
+
+    :param result: The response object from the API call.
+    :type result: TestResponse
+    :param content: The expected content to be present in the response.
+    :type content: str
+    """
     content = content.replace('"', "")
     assert content in result.json()
