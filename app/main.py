4548447b8faa66ed5aa485080494fbcdb084bceb
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-09-07 17:21:20 -0700

    add logging middleware

--- app/main.py
+++ app/main.py
@@ -85,6 +85,6 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         return response
 
-
+app.add_middleware(LoggingMiddleware)
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080)
