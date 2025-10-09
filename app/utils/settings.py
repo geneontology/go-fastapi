045cf1f5b44c37d2ab9272108f6f7c211540bbd2
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-10-09 10:03:30 -0700

    remove boilerplate access points for sparql server

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -12,7 +12,6 @@ import yaml
 CONFIG = path.join(path.dirname(path.abspath(__file__)), "../conf/config.yaml")
 
 golr_config = None
-sparql_config = None
 route_mapping = None
 logging.basicConfig(filename="combined_access_error.log", level=logging.INFO, format="%(asctime)s - %(message)s")
 logger = logging.getLogger()
@@ -66,16 +65,6 @@ def get_index_files(file_key: str = None) -> dict:
         with open(file_url, "r") as f:
             return json.load(f)
 
-
-def get_sparql_endpoint():
-    """Returns the SPARQL endpoint URL."""
-    global sparql_config
-    if sparql_config is None:
-        with open(CONFIG, "r") as f:
-            sparql_config = yaml.safe_load(f)
-    return sparql_config["sparql_url"]["url"]
-
-
 def get_user_agent():
     """Returns the user agent string."""
     name = "go-fastapi"
@@ -98,13 +87,6 @@ class ESOLR(Enum):
     """Enum for the GOLR URL."""
 
     GOLR = get_golr_config()["solr_url"]["url"]
-
-
-class ESPARQL(Enum):
-
-    """Enum for the SPARQL endpoint URL."""
-
-    SPARQL = get_sparql_endpoint()
 
 
 class ESOLRDoc(Enum):
